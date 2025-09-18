7b866bf384d1f64b4bb773707600dc3115474397
Sariafauzani <ajas2654@gmail.com>
2025-09-18 16:45:05 +0700

    input validation

--- KalkulatorBMI/bin/main.dart
+++ KalkulatorBMI/bin/main.dart
@@ -2,8 +2,12 @@ void main() {
   List<Map<String, dynamic>> riwayat = [];
 
   // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
 
   // Tampilkan riwayat
+  tampilkanRiwayat(riwayat);
 
 }
 
